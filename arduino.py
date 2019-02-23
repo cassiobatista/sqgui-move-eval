18f6396a5ff6bcea77fa4de7bdec3610b3ae02d6
Cassio Batista <cassio.batista.13@gmail.com>
2019-02-23 09:57:51 -0300

    header reafact on arduino serial codes

--- arduino.py
+++ arduino.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# https://stackoverflow.com/questions/20856518/navigate-between-widgets-using-arrows-in-qt
+#
+# author: feb 2019
+# Erick
+
 import time
 import serial
 import sys
+
 comport = serial.Serial('/dev/ttyACM0', 9600)
-
-
-
 comport.write(str.encode('@'))
 
 #VALUE_SERIAL=comport.readline()
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,11 +1,17 @@
-void setup(){
- Serial.begin(9600);
+/*
+ *
+ * author: feb 2019
+ * erick
+ */
+
+void setup()
+{
+	Serial.begin(9600);
 }
 
-void loop(){
-	if(Serial.available()>0){
-		if(Serial.read()==64){
+void loop()
+{
+	if(Serial.available() > 0) 
+		if(Serial.read() == 64)
 			Serial.println("ok");
-		}
-	}
 }
