180b4b6908aca74b8587c4ef5bd26086ff00bdbf
ErickModesto <erick.c.modesto@gmail.com>
2019-03-28 13:21:24 -0300

    Acho que tá funcionando dessa vez

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -25,62 +25,70 @@ void loop()
 			//Keyboard.write(218);
 			Serial.println("Recebi");
 			eog=1;
+
 			while(eog){
-				up = analogRead(0)*(5.0/1023.0);
+				up = analogRead(2)*(5.0/1023.0);
 				lr = analogRead(3)*(5.0/1023.0);
-				if(up>=2.0 && up_flag==0){
-					up_flag=1;
-					inicio = millis();
+				//if(up>=2.0 && up_flag==0){
+				//	up_flag=1;
+				//	inicio = millis();
+				//}
+
+				//while(up_flag==1){
+				//	up = analogRead(0)*(5.0/1023.0);
+				//	if(up<=1.8){
+				///		up_flag=0;
+				//		fim = millis();
+				//		tempo = fim - inicio;
+
+				//Serial.println(tempo);
+				//delay(1000);
+
+				//		if(tempo<180){
+				//			Serial.println("Blink");
+				//			delay(1000);
+				//		}
+
+				//		else if(tempo>200){
+				//			Serial.println("Up");
+				//			Keyboard.write(218);
+				//			eog=0;
+				//			delay(1000); 
+				//		}
+
+				//	}
+
+				//}
+
+
+				if(lr>=2.5){
+					Serial.println("Right");
+					Keyboard.write(215);
+					eog=0;
+					delay(1000);
 				}
-				while(up_flag==1){
-					up = analogRead(0)*(5.0/1023.0);
-					if(up<=1.8){
-						up_flag=0;
-						fim = millis();
-						tempo = fim - inicio;
-
-						//Serial.println(tempo);
-						//delay(1000);
-
-						if(tempo<180){
-							Serial.println("Blink is not a good movement");
-							//eog=0;
-							delay(1000);
-						}
-
-						else if(tempo>200){
-							Serial.println("Up");
-							Keyboard.write(218);
-							eog=0;
-							delay(1000); 
-						}
-
-					}
-
-
-					if(lr>=2.5){
-						Serial.println("Right");
-						Keyboard.write(215);
-						eog=0;
-						delay(1000);
-					}
-					else if(lr<=1.1){
-						Serial.println("Left");
-						Keyboard.write(216);
-						eog=0;
-						delay(1000);
-					}
-					if(up<=1.1){
-						Serial.println("Down");
-						Keyboard.write(215);
-						eog=0;
-						delay(1000);
-					}
-
+				else if(lr<=0.8){
+					Serial.println("Left");
+					Keyboard.write(216);
+					eog=0;
+					delay(1000);
+				}
+				else if(up<=1.1){
+					Serial.println("Down");
+					Keyboard.write(217);
+					eog=0;
+					delay(1000);
+				}
+				else if(up>=1.8){
+					Serial.println("up");
+					eog=0;
+					Keyboard.write(218);
+					delay(1000);
 
 				}
 
 			}
+
 		}
 	}
 
